e23ed0191b0f8051425b2e45a5d029249bb6b1d0
anushkadoshii <148350571+anushkadoshii@users.noreply.github.com>
2025-07-01 20:19:18 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -20,8 +20,6 @@ import os
 import tempfile
 from pathlib import Path
 # Initialize NLP model
-import spacy.cli
-spacy.cli.download("en_core_web_sm")
 nlp = spacy.load("en_core_web_sm")
 
 # GitHub Commits Tool
